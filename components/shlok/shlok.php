fd32267803acfde9e2c70ab48fd2b52707e9077d
Arabhaya <r.arabhaya@gmail.com>
2024-08-29 16:24:07 +0530

    changes

--- components/shlok/shlok.php
+++ components/shlok/shlok.php
@@ -1,13 +1,16 @@
-<div class="shlok-parent-cont">
-    <div class="shlok-container">
-        <div class="shlok-cont">
-            <?php
-            $shlok = "गङ्गातरङ्गरमणीयजटाकलापं गौरीनिरन्तरविभूषितवामभागम्। <br>
-                      नारायणप्रियमनङ्गमदापहारं वाराणसीपुरपतिं भज विश्वनाथम्॥ <br>
-                      श्रीविश्वनाथं शरणं प्रपद्ये।";
-
-            $shlok_detail = "~आदि शंकराचार्य~";
-            ?>
-            <p class="shlok">
-                <?php echo $shlok; ?>
-            </p>
+<?php
+echo '<div class="shlok-parent-cont">';
+echo '    <div class="shlok-container">';
+echo '        <div class="shlok-cont">';
+echo '            <p class="shlok">';
+echo '                गङ्गातरङ्गरमणीयजटाकलापं गौरीनिरन्तरविभूषितवामभागम्। <br>';
+echo '                नारायणप्रियमनङ्गमदापहारं वाराणसीपुरपतिं भज विश्वनाथम्॥ <br>';
+echo '                श्रीविश्वनाथं शरणं प्रपद्ये।';
+echo '            </p>';
+echo '            <p class="shlok-detail">';
+echo '                ~आदि शंकराचार्य~';
+echo '            </p>';
+echo '        </div>';
+echo '    </div>';
+echo '</div>';
+?>``
